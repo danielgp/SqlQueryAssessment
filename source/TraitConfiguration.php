b13ef043b8ba7f2ee3f536bbea9fb87e19d8644c
Daniel Popiniuc <danielpopiniuc@gmail.com>
2023-01-15 20:17:51 +0200

    eliminated a false raised issue

--- source/TraitConfiguration.php
+++ source/TraitConfiguration.php
@@ -41,7 +41,7 @@ trait TraitConfiguration
                         '<=',
                     ],
                     'Single'    => [
-                        '[^!<>]\=',
+                        '[^!<>\s]\=',
                         '\+',
                         '\-',
                     ],
